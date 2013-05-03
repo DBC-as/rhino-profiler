723085bce20f35a30a0a5694567ecdacbf693c1f
Rasmus Jensen <git-email@solsort.dk>
2013-05-03 14:46:02 +0200

    moved repository to github.com/DBCDK/rhino-profiler and left a README with link to the new url

--- run.sh
+++
@@ -1,2 +0,0 @@
-export JSJAR=`locate js.jar | head -n 1`
-javac -cp $JSJAR dk/dbc/rhinoprofiler/*.java && jar cf rhinoprofiler.jar dk/dbc/rhinoprofiler/*.class && java -classpath ./rhinoprofiler.jar:$JSJAR org.mozilla.javascript.tools.shell.Main -opt -1 run.js 
